e5330b34ed8f06775a3db90e115d559c0888fd44
Courdevelops <courdevelops@gmail.com>
2023-06-27 15:46:20 -0400

    about section done

--- slidemenu.js
+++ slidemenu.js
@@ -29,6 +29,9 @@
 // toggle the css class to slide in
 
 
+
+// HOME SECTION
+
 const hamburger = document.querySelector('.menuburger');
 
 const wholebutton = document.querySelector('.hamburgermobile');
@@ -37,18 +40,48 @@ const buttontext = document.querySelector('.closetext');
 
 const slide_menu_home = document.querySelector('.mobile-nav-home');
 
-
+// apply the click function to the wholebutton
 wholebutton.addEventListener('click', function () {
     // test print
     console.log("You've pressed the burger button");
     // toggle the is-gone class on the burger
     hamburger.classList.toggle('is-gone');
-    // toggle the is-active class on the whole button
+    // toggle the is-active class on the whole button to 
+        // show the X and the slide in menu
     wholebutton.classList.toggle('is-active');
-    // toggle the button text when the button is active
+    // toggle the X to is-shown when the button is active
     buttontext.classList.toggle('is-shown');
     // toggle the slide in menu css class to slide-in
     slide_menu_home.classList.toggle('slide-in');
 });
+
+
+// ABOUT SECTION
+
+const abouthamburger = document.querySelector('.menuburgerabout');
+
+const aboutwholebutton = document.querySelector('.about_hamburgermobile');
+
+const aboutbuttontext = document.querySelector('.about_closetext');
+
+const slide_menu_about = document.querySelector('.mobile-nav-about');
+
+
+// apply the click function to the wholebutton
+aboutwholebutton.addEventListener('click', function () {
+    // test print
+    console.log("You've pressed the burger button");
+    // toggle the is-gone class on the burger
+        // to make the burger dissapear
+    abouthamburger.classList.toggle('is-gone');
+    // toggle the is-active class on the whole button to 
+        // show the X and the slide in menu
+    aboutwholebutton.classList.toggle('is-active');
+    // toggle the X to is-shown when the button is active
+    aboutbuttontext.classList.toggle('is-shown');
+    // toggle the slide in menu css class to slide-in
+    slide_menu_about.classList.toggle('slide-in');
+});
+
 
 
